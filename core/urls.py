e563f3d3209d42261bb954990f3978125761eaef
sanjibpaul59 <sanjibpaul171@gmail.com>
2023-08-31 15:56:23 +0600

    Asset urls added

--- core/urls.py
+++ core/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# TODO: Add routes for apps
 urlpatterns = [
+    path('api/', include('assets.urls')),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('auth/', include('rest_framework.urls')),
 ]
