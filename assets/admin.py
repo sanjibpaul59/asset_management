037a6069600af4b3ba51b70d855bc8878ab0297f
sanjibpaul59 <sanjibpaul171@gmail.com>
2023-09-02 22:06:39 +0600

    Added models in the admin

--- assets/admin.py
+++ assets/admin.py
@@ -3,3 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from . import models
 admin.site.register(models.CustomUser)
+admin.site.register(models.Company)
+admin.site.register(models.Device)
+admin.site.register(models.ConditionLog)
